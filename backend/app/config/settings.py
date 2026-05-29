440ee3027518f56f9122312c988de426870cfcd2
Valentin <valentin.cristiannn@gmail.com>
2026-05-29 23:30:05 +0300

    Final  Version fixes

--- backend/app/config/settings.py
+++ backend/app/config/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 import os
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -166,7 +167,7 @@ REST_FRAMEWORK = {
 }
 
 # JWT
-from datetime import timedelta
+
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME':  timedelta(hours=1),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
